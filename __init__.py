4ed0d5a8994f2f499c53f5bcd6e102ad01a236ca
Nathaniel Sabanski <sabanski.n@gmail.com>
2022-05-18 23:52:54 -0700

    Added Prism code highlighter.

--- __init__.py
+++ __init__.py
@@ -123,8 +123,11 @@ class OutlineToHtmlCreator:
 	<head>
 		<title>{HTML_TITLE}</title>
 		<style>{HTML_STYLES}</style>
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/themes/prism-twilight.min.css" rel="stylesheet" />
 	</head>
 	<body>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/components/prism-core.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/plugins/autoloader/prism-autoloader.min.js"></script>
 """
 
 		HTML_FOOTER = f"""
